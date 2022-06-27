cf75fc374c1df95625e868b3a83db865df0e7a00
aidanliddiard <aidanliddiard@gmail.com>
2022-06-27 14:32:02 -0700

    redirect to oauth page working and test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,9 +7,11 @@ describe('gitty routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it.skip('should redirect to the github oauth page upon login', async () => {
+  it('should redirect to the github oauth page upon login', async () => {
     const res = await request(app).get('/api/v1/github/login');
-    expect(res.header.location).toMatch();
+    expect(res.header.location).toMatch(
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
+    );
   });
   afterAll(() => {
     pool.end();
